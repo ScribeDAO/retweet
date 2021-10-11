de76d9600b094cc29341867cab55f7935ea4ba7c
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-10-10 20:28:27 -0500

    feat: finalize form style

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,18 +1,62 @@
-import { GetStaticProps } from 'next'
-import { useSession } from 'next-auth/client'
-import { Image, Heading, FormControl, FormLabel, Flex } from '@chakra-ui/react'
-import { Select } from 'chakra-react-select'
-import { SummaryFormProps } from '../components/SubmitSummary'
-import Navbar from '../components/Navbar'
-import prisma from '../lib/db'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useSession } from 'next-auth/client'
+import {
+  Image,
+  Heading,
+  FormControl,
+  FormLabel,
+  Flex,
+  Button,
+} from '@chakra-ui/react'
+import { Select } from 'chakra-react-select'
+import Navbar from '../components/Navbar'
+import { useMutation, useQuery } from '../gqty'
 
-type Data = {
-  tags: SummaryFormProps['tags']
+const CategorySelection = () => {
+  const query = useQuery()
+  const [categories, setCategories] = useState<
+    Array<{ label: string; value: string }>
+  >([])
+
+  const [updateUserCategories, { isLoading, data, error }] = useMutation(
+    (mutation, args: Array<{ categories: string }>) => {
+      return mutation.updateUserCategories({ ...args })
+    },
+  )
+
+  const options = query!
+    .tags()!
+    .edges!.map((tag) => ({ label: tag?.node?.name, value: tag?.node?.id }))
+
+  const updateCategories = () => {
+    console.log(categories)
+  }
+
+  return (
+    <FormControl maxW="70%">
+      <Flex direction="column" marginTop="2rem">
+        <FormLabel>Select categories you are interested in</FormLabel>
+        <Flex justifyContent="center" alignItems="center">
+          <FormControl>
+            <Select
+              // @ts-ignore Docs say this exists and it works.
+              isMulti
+              options={options}
+              value={categories}
+              onChange={(e: any) => setCategories(e)}
+            />
+          </FormControl>
+          <Button ml="1rem" colorScheme="teal" onClick={updateCategories}>
+            Update
+          </Button>
+        </Flex>
+      </Flex>
+    </FormControl>
+  )
 }
 
-const Profile = ({ tags }: Data) => {
+const Profile = () => {
   const [session] = useSession()
   const router = useRouter()
 
@@ -22,7 +66,6 @@ const Profile = ({ tags }: Data) => {
     }
   }, [router, session])
 
-  const options = tags.map((tag) => ({ label: tag.name, value: tag.id }))
   return (
     <>
       <Navbar hideProfile />
@@ -40,26 +83,10 @@ const Profile = ({ tags }: Data) => {
         </Flex>
       </Flex>
       <Flex justifyContent="center">
-        <FormControl maxWidth="40rem" marginTop="2rem">
-          <FormLabel textAlign="center">
-            Select categories you are interested in
-          </FormLabel>
-          {/* @ts-ignore Docs say this exists and it works. */}
-          <Select isMulti options={options} />
-        </FormControl>
+        <CategorySelection />
       </Flex>
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  // get all the tags to choose from
-  const tags = await prisma.tag.findMany({ select: { id: true, name: true } })
-
-  return {
-    props: {
-      tags,
-    },
-  }
-}
 export default Profile
